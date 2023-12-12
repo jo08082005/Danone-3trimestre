e2cd83eaa1b3e68860139bd3dbc1d8a72c89e2aa
jo08082005 <130716134+jo08082005@users.noreply.github.com>
2023-12-12 19:15:45 -0300

    a

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 alert('ola mundo')
 function tocasompom(){ 
 document.querySelector("#som_tecla_pom").play();}
-document.querySelector(".tecla_pom").onclick = tocasompom ;
+document.querySelector(".tecla_pom").onclick = tocaSomPom;
